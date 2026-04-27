d0665a06b6b8fb13f0baa2381e38be180fb5415c
Yauheni Siomin <elkind2605@gmail.com>
2026-04-27 23:09:07 +0300

    add task description end documentation

--- src/variables_dataTypes_1/NumberSignChecker.java
+++ src/variables_dataTypes_1/NumberSignChecker.java
@@ -2,6 +2,14 @@ package variables_dataTypes_1;
 
 import java.util.Scanner;
 
+/*1. Проверка числа
+Ввести число и:
+
+если > 0 → “Положительное”
+если < 0 → “Отрицательное”
+если = 0 → “Ноль”*/
+
+
 public class NumberSignChecker {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -10,6 +18,12 @@ public class NumberSignChecker {
         checkNumberSign(number);
 
     }
+
+    /**
+     * метод для проверки числа
+     * @param number - вводимое пользователем число
+     */
+
     public static void checkNumberSign(int number) {
         if (number < 0) {
             System.out.println("Введено отрицательное число");
